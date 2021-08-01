1a21fe23bda9c3a696ec9e3e1d289c42de497be3
Sisi Vieira <cicy886@gmail.com>
2021-08-01 13:07:15 -0700

    updated files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 
 module.exports = {
@@ -19,7 +20,8 @@ module.exports = {
       title: 'Project Template',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
